09a534a32d8a7ebf77772ac49e2e1db213991d75
mpkbt <mpkbt@pop-os.localdomain>
2022-03-03 18:42:52 -0300

    optimization

--- graph.js
+++ graph.js
@@ -1,58 +1,83 @@
-import { Layout, Hex } from './dist/lib-module.js'
-export class Graph extends Layout{
+import { Layout, Hex } from "./dist/lib-module.js";
+export class Graph extends Layout {
   constructor(size, origin) {
-    super(Layout.flat, {x:size,y:size}, origin)
-    this.tiles = []
+    super(Layout.flat, { x: size, y: size }, origin);
+    this.tiles = [];
     // size - size of tile(pixels)
   }
-  
-  addTile(q,r,s=-q-r){
+
+  addTile(q, r, s = -q - r) {
     //TODO if this.hasTile(q,r,s) throw error
-    this.tiles.push(new Tile(q,r,s))
-    return this
+    this.tiles.push(new Tile(q, r, s));
+    return this;
   }
 
-  getTile(q,r,s){
-    return this.tiles.find(t=> t.q==q && t.r==r)
-
+  getTile(q, r, s) {
+    return this.tiles.find((t) => t.q == q && t.r == r);
   }
-  addPixel({x,y},value){
-    if (value == null){
-      throw 'value must not be null'
+  addPixel({ x, y }, value) {
+    if (value == null) {
+      throw "value must not be null";
     }
-    const {q,r,s} = this.pixelToHex({x,y}).round()
+    const { q, r, s } = this.pixelToHex({ x, y }).round();
 
-    let tile = this.getTile(q,r,s)
-    if(tile==null){
+    let tile = this.getTile(q, r, s);
+    if (tile == null) {
       //nao uso o addTile pra nao precisar usar o this.getTile dps
-      tile = new Tile(q,r,s)
-      this.tiles.push(tile)
+      tile = new Tile(q, r, s);
+      this.tiles.push(tile);
+    }
+    tile.addPixel(value);
+
+    return tile;
+  }
+  addData(data) {
+    const start = Date.now()
+    const { width, height } = data;
+    const size = this.size.x
+    let lastTile = null
+    let lastTilePos = {x:-1,y:-1}
+    const n = data[0].length
+
+    for (let i = 0; i < n; i++) {
+      if(i%10000==0){
+        console.log(`${(i/n*100).toFixed(1)}%`)
+      }
+      const x = i % width;
+      const y = Math.floor(i / width);
+      const value = data[0][i];
+      if(x-lastTilePos.x<size && y==lastTilePos.y){
+        lastTile.addPixel(value)
+        continue
+      }
+      const tile = this.addPixel({ x, y }, value);
+      const changed = tile !== lastTile
+      if (changed) {
+        lastTilePos = {x,y}
+        lastTile = tile
+      }
       
     }
-    tile.addPixel(value)
-
-    return this
+    console.log(`${n} pixels computed in ${((Date.now()-start)/1000).toFixed(2)}s`)
   }
 }
-export class Tile extends Hex{
-  constructor(q,r,s=-q-r){
-    super(q,r,s)
-    this.pixels = []
+export class Tile extends Hex {
+  constructor(q, r, s = -q - r) {
+    super(q, r, s);
+    this.pixels = [];
   }
-  addPixel(value){
-  // tirei o " new Pixel({x, y}, value) " pq n vou usar esse xy e seria mto caro
-    this.pixels.push(value)
+  addPixel(value) {
+    // tirei o " new Pixel({x, y}, value) " pq n vou usar esse xy e seria mto caro
+    this.pixels.push(value);
   }
-  get mean(){
-    return this.pixels.reduce((a,v)=>a+v,0)/this.pixels.length
-  }
-
-}
-export class Pixel{
-  constructor({x,y},value){
-    this.x = x
-    this.y = y
-    this.value = value
+  get mean() {
+    return this.pixels.reduce((a, v) => a + v, 0) / this.pixels.length;
   }
 }
-
+export class Pixel {
+  constructor({ x, y }, value) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+  }
+}
--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-import {plotData} from './plot.js'
-import {printHexagons} from './print-hex.js'
-import {Graph,Tile,Pixel} from './graph.js'
-import { Point, Hex } from './dist/lib-module.js'
+import { plotData } from "./plot.js";
+import { printHexagons } from "./print-hex.js";
+import { Graph, Tile, Pixel } from "./graph.js";
+import { Point, Hex } from "./dist/lib-module.js";
 
-const canvas = document.getElementById('plot')
-const graphCanvas = document.getElementById('graph')
-const w = canvas.width = graphCanvas.width = window.innerWidth  
-const h = canvas.height = graphCanvas.height = window.innerHeight 
+const canvas = document.getElementById("plot");
+const graphCanvas = document.getElementById("graph");
+const w = (canvas.width = graphCanvas.width = window.innerWidth);
+const h = (canvas.height = graphCanvas.height = window.innerHeight);
 
-async function loadTiff(){
+async function loadTiff() {
   // using local ArrayBuffer
-  const response = await fetch('./tiffs/small.tif');
+  const response = await fetch("./tiffs/small.tif");
   const arrayBuffer = await response.arrayBuffer();
   const tiff = await GeoTIFF.fromArrayBuffer(arrayBuffer);
-  return tiff
+  return tiff;
 }
 
-async function main(){
-  
+async function main() {
+  console.log('loading raster')
   const tiff = await loadTiff();
   const image = await tiff.getImage();
   const data = await image.readRasters();
 
-  const size = 40
-  const origin = {x:0,y:0}
-  const graph = new Graph(size,origin)
-  const {width, height} = data
-  for(let i in data[0]){
-    //TODO: da pra otimizar, com ele checando se o tile nao mudou e dar tile.addPixel
-    const x = i % width
-    const y = Math.floor(i/width)
-    const value = data[0][i]
-    graph.addPixel({x,y},value)
-  }
+  const size = 50;
+  const origin = { x: 0, y: 0 };
+  const graph = new Graph(size, origin);
+  console.log('computing graph...')
+  graph.addData(data);
 
   //const xy = graph.hexToPixel(new Hex(0,-1,1))
   //
@@ -41,66 +35,66 @@ async function main(){
   //TODO: mudar escala de ciza pra escala do plotty
   //
 
-  drawTiff(canvas,data)
-  drawGraph(graphCanvas,graph)
+  console.log('rendering image')
+  drawTiff(canvas, data);
+  console.log('rendering graph')
+  drawGraph(graphCanvas, graph);
 
   var slider = document.getElementById("opacity");
-  slider.addEventListener( 'input',(e)=> {
-    graphCanvas.style.opacity = e.target.value
-  })
-
+  slider.addEventListener("input", (e) => {
+    graphCanvas.style.opacity = e.target.value;
+  });
 }
-function drawTiff(canvas,data){
-  const {width, height} = data
-  const ctx = canvas.getContext('2d')
-  const newData = []
-  for (let i in data[0]){
-    const v = data[0][i]
-    const x = Math.round((v+1)/2*255)
-    newData.push(x)
-    newData.push(x)
-    newData.push(x)
-    newData.push(255)
-
+function drawTiff(canvas, data) {
+  const { width, height } = data;
+  const ctx = canvas.getContext("2d");
+  const newData = [];
+  for (let i in data[0]) {
+    const v = data[0][i];
+    const x = Math.round(((v + 1) / 2) * 255);
+    newData.push(x);
+    newData.push(x);
+    newData.push(x);
+    newData.push(255);
   }
 
-  ctx.putImageData(new ImageData(new Uint8ClampedArray(newData),width,height),0,0)
-
+  ctx.putImageData(
+    new ImageData(new Uint8ClampedArray(newData), width, height),
+    0,
+    0
+  );
 }
-function drawGraph(canvas,graph){
+function drawGraph(canvas, graph) {
   const hexs = graph.tiles.map((x) => {
     return {
       points: graph.polygonCorners(x),
-      fill: Math.round((x.mean+1)/2*255),
+      fill: Math.round(((x.mean + 1) / 2) * 255),
     };
   });
 
-  const ctx = canvas.getContext('2d')
-  ctx.lineWidth = 5
-  ctx.strokeStile = 'black'
-  hexs.forEach(hex=>{
-    drawHex(ctx,hex.points,hex.fill)
-  })
-
+  const ctx = canvas.getContext("2d");
+  ctx.lineWidth = 5;
+  ctx.strokeStile = "black";
+  hexs.forEach((hex) => {
+    drawHex(ctx, hex.points, hex.fill);
+  });
 }
 
-function drawHex(ctx, points, fill=null){
+function drawHex(ctx, points, fill = null) {
   ctx.beginPath();
   for (var i = 0; i < 6; i++) {
-    ctx.lineTo(points[i].x,points[i].y);
+    ctx.lineTo(points[i].x, points[i].y);
   }
   ctx.closePath();
-  if (fill===null){
+  if (fill === null) {
     ctx.stroke();
-    return
+    return;
   }
   ctx.stroke();
-  ctx.fillStyle = `rgb(${fill},${fill},${fill})`
-  ctx.closePath()
-  ctx.fill()
-
+  ctx.fillStyle = `rgb(${fill},${fill},${fill})`;
+  ctx.closePath();
+  ctx.fill();
 }
 
 //printHexagons(canvas)
-main()
-
+main();
